30d1a4edbc62d6a9a5139d08e76539a4d856ed68
wanderingduc <dkdanid@outlook.com>
2025-01-10 20:46:08 +0100

    Add action and view for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,25 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.expect(user: [ :email_address, :username, :password, :password_confirmation, :role ])
   end
 end
